5666d85fdc27a877fdad350e6157d29af388c85d
LLLZhao <zhaolinhui@10.255.255.137>
2017-03-02 11:21:35 -0400

    a3-stage2 to add at least 2 implement to check email

--- src/main/java/email_validator/emailValidator.java
+++ src/main/java/email_validator/emailValidator.java
@@ -10,15 +10,23 @@ public class emailValidator {
         
         atIndex = mail.indexOf('@', 0);
         fullIndex = mail.indexOf('.', 0);
-        //To determine . behind @
+        
+        //Determine contains at least one .
         if(	mail.contains("."))
         	count++;
+        
         //To determine only one @ in email address
         if(mail.indexOf('@', 0) == mail.lastIndexOf('@')) 
             count++;
-
-        return count;
-    
+        //To determine only 2 or 3 characters behind .
+        if ((mail.length() - fullIndex -1 == 2) || (mail.length() - fullIndex -1 == 3)){
+            count++;
+        }
+        //To determine . behind @
+        if(atIndex < fullIndex) {
+            count++;
+        }       
+        return count;  
     }
  
 }
--- src/test/java/email_validator/email_validator_test.java
+++ src/test/java/email_validator/email_validator_test.java
@@ -5,11 +5,11 @@ import org.junit.Test;
 import email_validator.emailValidator;
 
 public class email_validator_test {
-//test
+
 	@Test
 	public void test() {
 		emailValidator test = new emailValidator();
-		int result = test.validator("linhui@dal.ca");
-		assertEquals(2,result);
+		int result = test.validator("linhui@dal.com");
+		assertEquals(4,result);
 	}
 }
